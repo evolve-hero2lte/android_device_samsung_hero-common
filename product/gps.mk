6b9b2c3b51b8ee3b334495cdd65cc4acd3587a51
deed821 <deed821@gmail.com>
2017-08-02 13:59:50 +1000

    hero: cleanup source a little more

--- product/gps.mk
+++ product/gps.mk
@@ -3,6 +3,7 @@ DEVICE_PATH := device/samsung/hero-common
 # Configs
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+        $(DEVICE_PATH)/configs/gps/lhd.conf:system/etc/lhd.conf \
 	$(DEVICE_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
 # Init Resources
